595a05ca171397f3362ed11344ab28b8a8a3c74f
Efficient Software <solutions@efficientsoftware.io>
2021-09-16 16:23:14 -0600

    merge

--- test/goal_test.dart
+++ test/goal_test.dart
@@ -32,10 +32,13 @@ void main () {
   });
 
 
+<<<<<<< HEAD
   test('test no two goals are the same', () {
     Goal goal1 = createTestGoal();
     Goal goal2 = createTestGoal();
     expect(goal1 == goal2, false);
   });
 
+=======
+>>>>>>> parent of 10cff05 (added unique ids)
 }
