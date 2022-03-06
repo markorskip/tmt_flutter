8fd65ccc7a659cf3a8809f6535ba458e36bc2ab1
skiptheviking <markorskip@pm.me>
2022-03-05 20:09:44 -0500

    added todo for task moving:

--- lib/view/dialog/move_goal_dialog.dart
+++ lib/view/dialog/move_goal_dialog.dart
@@ -22,6 +22,11 @@ class _MoveGoalDialogState extends State<MoveGoalDialog> {
 
   Goal? dropdownValue;
 
+  // TODO isntead of this dialog, allow a user to drag and drop a slideable
+  // when the hold to drag and drop, an option appears to move up
+  // then can drop on top of another to drop into
+  // and then can reoder
+
   List<Goal> getGoalsInDropDown() {
     List<Goal> list = [];
     if (widget.grandParentGoal != null) {
